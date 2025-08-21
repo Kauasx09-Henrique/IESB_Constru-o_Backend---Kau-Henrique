2c8b8f18c86f50c1adfc2b9da3d50818adb23c2c
unknown <24114290095@ACADEMICO.IESB.BR>
2025-08-20 21:36:48 -0300

    Adiciona cálculo de notas e corrige lógica de condições if/else para determinar menção do aluno

---
+++ Praticas/01-Primeiro-projeto/index.js
@@ -0,0 +1,38 @@
+console.log("Rodando meu primeiro projeto em node");
+
+let nome = "Kauã";
+let sobre = "Henrique"
+
+console.log("Nome completo eh", nome +  " " + sobre  );
+
+let ano = 2005
+
+
+const idadeatual = (ano) => new Date().getFullYear() - ano
+
+console.log("sua idade eh", idadeatual(ano))
+
+let anovelho =2075 
+
+const idadevelha = (ano, anovelho) =>  anovelho - ano
+console.log("sua idadem em 2075", idadevelha(ano, anovelho))
+
+
+let exercicios = 0.5;
+let trabalho = 0.5;
+let prova = 2;
+let notas = 1;
+
+const notac = exercicios + trabalho + prova + notas;
+console.log("Suas notas total são:", notac);
+
+
+if (notac >= 9) {
+    console.log("Parabéns, menção máxima!", notac);
+} else if (notac >= 7) {
+    console.log("Parabéns, sua menção é boa:", notac);
+} else if (notac >= 5) {
+    console.log("Parabéns, você passou com a média mínima:", notac);
+} else {
+    console.log("Infelizmente você não atingiu a média:", notac);
+}
